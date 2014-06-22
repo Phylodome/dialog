073a07e31f50f9eecb626d40594447ab591ba56c
Jakub Strojewski <jakub.strojewski@gmail.com>
2014-06-22 13:49:22 +0200

    Refactor 'dialogManager'

--- dist/tri-angular-dialog.js
+++ dist/tri-angular-dialog.js
@@ -262,7 +262,7 @@ mod.provider('dialogManager', function () {
         hideClass: 'hide'
     };
 
-    var _getConfigData = function (initialData) {
+    var _getDialogConfigData = function (initialData) {
         return {
             controller: initialData.controller || false,
             dialogClass: initialData.dialogClass || '',
@@ -280,7 +280,7 @@ mod.provider('dialogManager', function () {
             throw new Error('dialog.DialogData() - initialData must contain defined "templateUrl"');
         }
 
-        var configData = _getConfigData(initialData);
+        var configData = _getDialogConfigData(initialData);
 
         if (initialData.dynamicParams) {
             configData.dynamicParams = initialData.dynamicParams;
@@ -303,52 +303,57 @@ mod.provider('dialogManager', function () {
             return this;
         },
 
-        $get: [
-            '$rootScope',
-            function ($rootScope) {
-                return {
+        $get: ['$rootScope', function ($rootScope) {
+
+            var DialogManager = function DialogManager() {
+                return angular.extend(this, {
                     dialogs: [],
+                    cfg: _config
+                });
+            };
 
-                    cfg: _config,
+            angular.extend(DialogManager.prototype, {
 
-                    hasAny: function (namespace) {
-                        return this.dialogs.filter(function (dialog) {
-                            return dialog.namespace === namespace;
-                        }).length > 0;
-                    },
+                hasAny: function (namespace) {
+                    return this.dialogs.filter(function (dialog) {
+                        return dialog.namespace === namespace;
+                    }).length > 0;
+                },
 
-                    getUpperDialog: function () {
-                        var count = this.dialogs.length;
-                        return count > 0 && this.dialogs[count - 1];
-                    },
+                getUpperDialog: function () {
+                    var count = this.dialogs.length;
+                    return count > 0 && this.dialogs[count - 1];
+                },
 
-                    registerDialog: function (dialog) {
-                        dialog.label = this.dialogs.push(dialog) - 1;
-                        return dialog;
-                    },
+                registerDialog: function (dialog) {
+                    dialog.label = this.dialogs.push(dialog) - 1;
+                    return dialog;
+                },
 
-                    unregisterDialog: function (label) {
-                        var dialog = this.dialogs[label];
-                        if (dialog && dialog.label === label) {
-                            delete dialog.label;
-                            this.dialogs.splice(label, 1);
-                            return true;
-                        }
-                        return false;
-                    },
-
-                    triggerDialog: function (data) {
-                        data = data || {};
-                        $rootScope.$emit(
-                            (data.namespace ? data.namespace + '.' : 'main.') + 'dialog.open',
-                            this.registerDialog(
-                                new DialogData(data)
-                            )
-                        );
+                unregisterDialog: function (label) {
+                    var dialog = this.dialogs[label];
+                    if (dialog && dialog.label === label) {
+                        delete dialog.label;
+                        this.dialogs.splice(label, 1);
+                        return true;
                     }
-                };
-            }
-        ]
+                    return false;
+                },
+
+                triggerDialog: function (data) {
+                    data = data || {};
+                    $rootScope.$emit(
+                        (data.namespace ? data.namespace + '.' : 'main.') + 'dialog.open',
+                        this.registerDialog(
+                            new DialogData(data)
+                        )
+                    );
+                    return this;
+                }
+            });
+
+            return new DialogManager();
+        }]
     };
 });
 
